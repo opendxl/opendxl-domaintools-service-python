baa61b971a4858ab8cec86a604879b465d6bc004
christsmith-mcafee <csmith5@mcafee.com>
2017-07-14 15:54:22 -0700

    Removed unnecessary files

--- config.bat
+++
@@ -1,6 +0,0 @@
-copy /y c:\temp\dxlclient-python-sdk-3.1.0.585\sample\dxlclient.config config
-copy /y c:\temp\dxlclient-python-sdk-3.1.0.585\sample\dxlclient.config sample
-copy /y c:\temp\dxlclient-python-sdk-3.1.0.585\sample\*.crt sample
-copy /y c:\temp\dxlclient-python-sdk-3.1.0.585\sample\*.crt config
-copy /y c:\temp\dxlclient-python-sdk-3.1.0.585\sample\*.key config
-copy /y c:\temp\dxlclient-python-sdk-3.1.0.585\sample\*.key sample
